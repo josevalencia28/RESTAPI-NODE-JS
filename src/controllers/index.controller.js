c4e7c6c85d3723437274413e7e11816c6bc85d95
Jose Valencia Ariza <jvalencia@informatica.local>
2022-11-23 17:20:30 -0500

    Jose Luis Valencia

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,32 +1,25 @@
 const { response } = require('express');
 const validatorHandler = require('./../middleware/validador.handler');
 const { createUsersSchemas,  updateUsersSchemas, deleteUsersSchemas} = require('./../schemas/users.schemas')
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'Jose2808',
-    database: 'firsapi',
-    port: '5432'
-});
+const conexion = require('../../database/conexion');
+const {consultarUsers, consultarUsersById, CreateUsers, DeleteUsersById,updateUsersM} = require('./../model/modelo.query');
 
 
 const getUsers = async (rq, res) => {
-    const response = await pool.query('SELECT * FROM users');
+    const response = await consultarUsers();
     res.status(200).json(response.rows);
 };
 
 const getUserById = async (req, res) => {
     const id = req.params.id;
-    const response = await pool.query( 'SELECT * FROM users WHERE id = $1 ', [id]);
+    const response = await consultarUsersById(id);
     res.json(response.rows);
 }
 
 const createUsers = async (req, res) => {
-    validatorHandler(createUsersSchemas, 'body')
+  //  validatorHandler(createUsersSchemas, 'body')
     const { name, email} = req.body;
-    const response = await pool.query('INSERT INTO users(name,email) VALUES ($1, $2)', [name, email]);
+    const response = await CreateUsers(name, email);
     console.log(response);
         res.json({
             message: 'User added successfully',
@@ -38,16 +31,16 @@ const createUsers = async (req, res) => {
 
 const deleteUsers = async (req, res) => {
     const id  = req.params.id;
-    const response = await pool.query('DELETE FROM users WHERE id = $1',[id]);
-    console.log(response);
+    const response = await DeleteUsersById(id);
+   console.log(response);
     res.json(`User ${id} deleted successfully`);
 };
     
 const updateUsers = async (req, res) => {
     const id = req.params.id;
     const { name, email} = req.body;
-    const response = await pool.query('UPDATE users SET name = $1,email = $2 WHERE id = $3 ',[name, email, id]);
-    console.log(response);
+    const response = await updateUsersM(name, email,id);
+  console.log(response);
     res.json({
         message: 'Userupdate successfully',
         body:{
---
+++ src/model/modelo.query.js
@@ -0,0 +1,82 @@
+const conexion = require("../../database/conexion");
+
+const consultarUsers = async () => {
+  let response;
+
+  try {
+    const query = `SELECT * FROM users`;
+    response = await conexion.query(query);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return response;
+};
+
+const consultarUsersById = async (id) => {
+  let response;
+
+  try {
+
+    const query = `SELECT * FROM users WHERE id = $1`;
+    const params = [id]
+
+    response = await conexion.query(query, params)
+
+  } catch (error) {
+    console.log(error);
+  }
+  
+  return response;
+};
+
+const CreateUsers = async( name, email) => {
+    let response;
+    
+    try {
+        const query = `INSERT INTO users(name, email) VALUES ($1, $2)`;
+        const params = [name, email]
+        response = await conexion.query(query,params)
+    } catch (error) {
+        console.log(error);
+    }
+    return response;
+};
+
+
+const DeleteUsersById = async(id) => {
+  let response;
+
+  try {
+    const query = `DELETE FROM users WHERE id = $1`;
+    const params = [id]
+    response = await conexion.query(query,params)
+    
+  } catch (error) {
+    console.log(error);
+  }
+  return response;
+};
+
+const updateUsersM = async(name, email, id) => {
+  let response;
+    try {
+      const query = `UPDATE users SET name = $1,email = $2 WHERE id = $3`;
+      const params = [name, email, id]
+      response = await conexion.query(query, params)
+      
+    } catch (error) {
+      console.log(error);   
+    }
+    return response;
+}
+
+
+
+module.exports = {
+  consultarUsers,
+  consultarUsersById,
+  CreateUsers,
+  DeleteUsersById,
+  updateUsersM
+};
